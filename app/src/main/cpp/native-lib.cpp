bd72bb26cb58e4f53e5c2ccdd97ccde30d0ea998
Double0101 <double0101@foxmail.com>
2017-09-02 16:27:53 +0800

    fds

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -3,12 +3,23 @@
 //
 #include <jni.h>
 #include <string>
+#include <opencv2/core.hhp>
 
-extern "C"
+extern "C" {
 jstring
 Java_com_sjgsu_ai_cameratest_TextFragment_getString(JNIEnv *env, jobject thiz)
 {
     return env->NewStringUTF("Hello world");
+}
+
+jstring
+Java_com_sjgsu_ai_cameratest_TextFragment_testOpenCV(JNIEnv *env, jobject thiz, jlong addrGray, jlong addrRgba) {
+    cv::Rect();
+    cv::Mat();
+    std::string hello2 = "Opencv test success";
+    return env->NewStringUTF(hello2.c_str())
+}
+
 }
 
 
--- app/src/main/java/com/sjgsu/ai/cameratest/TextFragment.java
+++ app/src/main/java/com/sjgsu/ai/cameratest/TextFragment.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import org.opencv.android.OpenCVLoader;
+
 /**
  * Created by double on 17-8-23.
  */
@@ -16,6 +18,7 @@ public class TextFragment extends Fragment {
 
     static {
         System.loadLibrary("native-lib");
+        System.loadLibrary("opencv_java3");
     }
 
     private TextView mTextView;
@@ -25,8 +28,17 @@ public class TextFragment extends Fragment {
         View view = inflater.inflate(R.layout.text_layout, container, false);
         mTextView = (TextView) view.findViewById(R.id.text_view);
         mTextView.setText(getString());
+
+        if (!OpenCVLoader.initDebug()) {
+            mTextView.setText(mTextView.getText() + "\n OpenCVLoader initDebug() not work");
+        } else {
+            mTextView.setText(mTextView.getText() + "\n OpenCVLoader initDebug() working");
+            mTextView.setText(mTextView.getText() + "\n" + testOpenCV(0l, 0l));
+        }
+
         return view;
     }
 
     public native String getString();
+    public native String testOpenCV(long matAddrGr, long matAddrRgba);
 }
